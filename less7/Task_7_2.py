5ec01310c64dfdb9f2c097ff760201b2f6f96b7a
Maxim963 <m.m.n.1984@yandex.ru>
2020-03-22 13:50:16 +0300

    Task_7_2

---
+++ less7/Task_7_2.py
@@ -0,0 +1,40 @@
+# Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+# заданный случайными числами на промежутке [0; 50).
+# Выведите на экран исходный и отсортированный массивы.
+import random
+
+min = 0
+max = 51
+arr = [i for i in range(min, max)]
+random.shuffle(arr)
+print(arr)
+
+
+def merge_sort(arr):
+    n = len(arr)
+    if n <= 1:
+        return arr
+    else:
+        middle = int(len(arr) / 2)
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+        return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0] <= right[0]:
+            result.append(left[0])
+            left = left[1:]
+        else:
+            result.append(right[0])
+            right = right[1:]
+    if len(left) > 0:
+        result += left
+    if len(right) > 0:
+        result += right
+    return result
+
+
+print(merge_sort(arr))
